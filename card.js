25a41313d70844979ed192aec66cee34dbe9b4e1
unknown <dianarace@hotmail.com>
2022-01-13 13:48:20 +0000

    moved card flip to own file

---
+++ card.js
@@ -0,0 +1,5 @@
+const card = document.querySelector(".imageInner");
+
+card.addEventListener("click", function (e) {
+    card.classList.toggle('is-flipped');
+});
--- index.js
+++ index.js
@@ -7,8 +7,3 @@ function myFunction() {
     }
 }
 
-const card = document.querySelector(".imageInner");
-
-card.addEventListener("click", function (e) {
-    card.classList.toggle('is-flipped');
-});
